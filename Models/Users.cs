711c8375003160476f9df475b7b146fb1cdfd7ca
Derjai <ojaider@uninorte.edu.co>
2024-11-23 00:54:43 -0500

    User Model modified
    Added data annotations

--- Models/Users.cs
+++ Models/Users.cs
@@ -1,9 +1,21 @@
-﻿namespace task_manager.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace task_manager.Models
 {
     public class Users
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public required int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
         public required string Email { get; set; }
+
+        [Required]
+        [MinLength(8)]
         public required string Password { get; set; }
+
+        public ICollection<Tasks>? Tasks { get; set; }
     }
 }
