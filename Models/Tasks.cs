b6e8d30f80b0ed1890aabcb9a96a1b168682b5be
Derjai <ojaider@uninorte.edu.co>
2024-11-22 22:18:31 -0500

    Added 'tasks'
    Added model class for storing tasks in the database with the following data:
    - Id
    - userId
    - Title
    - Description
    - TaskStatus
    - TaskPriority

---
+++ Models/Tasks.cs
@@ -0,0 +1,27 @@
+﻿namespace task_manager.Models
+{
+    public class Tasks
+    {
+        public required int Id { get; set; }
+        public required int userId { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public required Status TaskStatus { get; set; }
+        public required Priority TaskPriority { get; set; }
+
+        public enum Priority
+        {
+            Low,
+            Medium,
+            High
+        }
+
+        public enum Status
+        {
+            Pending,
+            InProgress,
+            Done
+        }
+
+    }
+}
